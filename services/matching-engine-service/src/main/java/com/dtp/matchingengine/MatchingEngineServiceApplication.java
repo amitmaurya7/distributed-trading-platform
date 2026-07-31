919a9cf45f688f2745296f9656c7b03ad65b69a3
amit-kumar-maurya <amitmauryamm777@gmail.com>
2026-07-31 14:57:28 +0530

    feat: missing file

---
+++ services/matching-engine-service/src/main/java/com/dtp/matchingengine/MatchingEngineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dtp.matchingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingEngineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingEngineServiceApplication.class, args);
+	}
+
+}
